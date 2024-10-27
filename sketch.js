be01d0b81cd8fa5cda5ac0a1d67eb410bea8920f
Omer <62906173+l0rdyolo@users.noreply.github.com>
2024-10-27 23:11:12 +0300

    slider and play button added

--- sketch.js
+++ sketch.js
@@ -1,25 +1,49 @@
- 
- let sound, visualizer;
- 
- function preload() {
-     sound = loadSound('pain.mp3');
- }
- 
- function setup() {
-     createCanvas(800, 400);
-     visualizer = new SoundVisualizer(3);
-     visualizer.loadSound(sound);
-     sound.play();
- }
- 
- function draw() {
-     background(0);
-     visualizer.visualize();
- 
-     let smoothSpeed = visualizer.getSmoothSpeed();
- 
-     fill(255);
-     textSize(20);
-     text(`Speed: ${nf(smoothSpeed, 1, 0)} units`, 10, 30);
- }
- 
+let sound, visualizer;
+let playButton;
+let smoothingSlider;
+
+function preload() {
+  sound = loadSound('pain.mp3');
+}
+
+function setup() {
+  createCanvas(window.innerWidth, 400);
+  
+  visualizer = new SoundVisualizer(3); 
+  visualizer.loadSound(sound);
+  
+  playButton = createButton('Play');
+  playButton.position(10, height + 10);
+  playButton.mousePressed(togglePlay);
+
+  smoothingSlider = createSlider(1, 10, 3);
+  smoothingSlider.position(100, height + 10);
+  smoothingSlider.style('width', '200px');
+}
+
+function draw() {
+  background(0);
+  
+  let smoothingWindow = smoothingSlider.value();
+  visualizer.setSmoothingWindow(smoothingWindow); 
+  visualizer.visualize(); 
+
+  let smoothSpeed = visualizer.getSmoothSpeed();
+  fill(255);
+  textSize(20);
+  text(`Speed: ${nf(smoothSpeed, 1, 0)}`, 10, 30);
+
+  fill(255);
+  textSize(10);
+  text(`Smoothing Window: ${smoothingWindow}`, 10, 50 ,120,120);
+}
+
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+    playButton.html('Play');
+  } else {
+    sound.play();
+    playButton.html('Pause');
+  }
+}
